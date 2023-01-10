9394b7258dd8cb96c44e653d40a8b6b06cf01936
mohammed ramadan <ramadanmohammed502@gmail.com>
2023-01-10 23:44:07 +0200

    dsad

--- utils/URL.jsx
+++ utils/URL.jsx
@@ -1,5 +1,3 @@
-const dev = process.env.NODE_ENV !== "production";
-const URL = dev
-        ? "http://localhost:3000"
-        : process.env.API_URL;
+// const dev = process.env.NODE_ENV !== "production";
+const URL = process.env.API_URL;
 export default URL
